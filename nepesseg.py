22d8a4ce23322fbf1a2cbb9d1befcfdd2bea032e
marcellmazsu <marcellmazsu@gmail.com>
2026-09-15 14:37:46 +0200

    1 es kesz

--- nepesseg.py
+++ nepesseg.py
@@ -8,6 +8,9 @@
 [3]
 [4]
 '''
+varosok = []
+
+lefutars = 0
 with open("nepesseg/repository/lakossag_2025.csv","r",encoding="UTF-8") as fajl:
     fajl.readline()
     for sor in fajl:
@@ -19,7 +22,7 @@ with open("nepesseg/repository/lakossag_2025.csv","r",encoding="UTF-8") as fajl:
             "ferfi" : int(adat_sor[3].replace(" ","")),
             "no" : int(adat_sor[4].replace(" ",""))
         }
-
+        varosok.append(adatok)
 print("[1] Megye adatai")
 print("[2] Telepules tipusa")
 print("[3]")
@@ -27,5 +30,37 @@ print("[4]")
 menupont = int(input("Valaszon egy opciot:"))
 if menupont == 1:
     megyekod_betu = input("Valaszon egy megyekodot (BUD/BAC/BAR/BEK/BOR/CSO/FEJ/GYO/HAJ/HEV/KOM/NOG/PES/SOM/SZA/SZO/TOL/VAS/VES/ZAL)")
-    for adat in adatok:
-  
+    for varos in varosok:
+            if varos["megyekod"] == megyekod_betu:
+                print(varos["telepules"],end=";")
+                print(varos["tipus"],end=";")
+                nepesseg = varos["ferfi"] + varos["no"]               
+                print(nepesseg)
+                lefutars += 1
+            if lefutars == 12:
+                lefutars = 0
+                break
+    for i in range(12):
+        del varosok[0]
+                
+
+    while True:
+        tovabb = input("Sokozzel tovabb/x -szel kilep")
+        if tovabb == " ":
+                for varos in varosok:
+                    if varos["megyekod"] == megyekod_betu:
+                        print(varos["telepules"],end=";")
+                        print(varos["tipus"],end=";")
+                        nepesseg = varos["ferfi"] + varos["no"]
+                        print(nepesseg)
+                        lefutars += 1
+                    if lefutars == 12:
+                        lefutars = 0
+                        break
+                for i in range(12):
+                    del varosok[0]
+                        
+        elif tovabb == "x":
+            break
+        else:
+            tovabb = input("Sokozzel tovabb/x -szel kilep")
